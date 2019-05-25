854128caaf6b7a42b7f547bec24cc3bbb4b0e31b
Ward Cunningham <ward@c2.com>
2019-05-25 15:28:15 -0700

    find server-source

--- client/detect.js
+++ client/detect.js
@@ -10,14 +10,32 @@
       .replace(/\*(.+?)\*/g, '<i>$1</i>');
   };
 
-  emit = ($item, item) => {
-    return $item.append(`
-      <p style="background-color:#eee;padding:15px;">
-        ${expand(item.text)}
-      </p>`);
+  function parse(text, $item) {
+    let candidates = $(`.item:lt(${$('.item').index($item)})`)
+    let who = candidates.filter('.server-source')
+    if (who.size()) {
+      let sources = $.map(who, (source) => {
+        let service = source.service()
+        console.log(service)
+        return service.parse.output
+      })
+      return {output: expand(text)+"<p class=caption>"+sources.join('<hr>')+"</p>"}
+    }
+    return {output: expand(text)+"<p class=caption>can't find service to monitor</p>"}
+  }
+
+  function emit($item, item) {
+
+    let parsed = parse(item.text, $item)
+    $item.append(`
+      <div style="background-color:#eee; padding:15px; margin-block-start:1em; margin-block-end:1em;">
+        ${parsed.output}
+        <center><button disabled>wait</button></center>
+      </div>`);
   };
 
-  bind = function($item, item) {
+
+  function bind($item, item) {
     return $item.dblclick(() => {
       return wiki.textEditor($item, item);
     });
