504446a7f7fe77967c6ac01ddd0ad4b68876dce2
Ward Cunningham <ward@c2.com>
2019-05-30 16:18:18 -0700

    start/stop logic

--- client/detect.js
+++ client/detect.js
@@ -1,14 +1,13 @@
 
 (function() {
-  var bind, emit, expand;
 
-  expand = text => {
+  function expand (text) {
     return text
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
       .replace(/\*(.+?)\*/g, '<i>$1</i>');
-  };
+  }
 
   function parse(text, $item) {
     let output = expand(text)
@@ -27,7 +26,6 @@
   }
 
   function emit($item, item) {
-
     let parsed = parse(item.text, $item)
     $item.append(`
       <div style="background-color:#eee; padding:15px; margin-block-start:1em; margin-block-end:1em;">
@@ -38,10 +36,38 @@
 
 
   function bind($item, item) {
-    return $item.dblclick(() => {
+    $item.dblclick(() => {
       return wiki.textEditor($item, item);
     });
-  };
+
+    let $button = $item.find('button')
+    let parsed = parse(item.text)
+
+    function action(command) {
+      $button.prop('disabled',true)
+      $page = $item.parents('.page')
+      if($page.hasClass('local')) {
+        return
+      }
+      slug = $page.attr('id').split('_')[0]
+      $.ajax({
+        type: "POST",
+        url: `/plugin/detect/${slug}/id/${item.id}`,
+        data: JSON.stringify(command),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+          $button.text((data.status == 'active') ? 'stop' : 'start')
+          $button.prop('disabled',false)
+        },
+        failure: function(err) {
+          console.log(err)
+        }
+      })
+    }
+    $button.click(event => action({action:$button.text(),schedule:parsed.schedule}))
+    action({})
+  }
 
   if (typeof window !== "undefined" && window !== null) {
     window.plugins.detect = {emit, bind};
--- server/server.js
+++ server/server.js
@@ -1,17 +1,62 @@
 // detect plugin, server-side component
 // These handlers are launched with the wiki server.
 
-(function() {
-  const startServer = params => {
-    var app = params.app,
-        argv = params.argv;
+function startServer (params) {
+  var app = params.app,
+      argv = params.argv;
 
-    return app.get('/plugin/detect/:thing', (req, res) => {
-      var thing = req.params.thing;
-      return res.json({thing});
-    });
-  };
+  var detectors = {} // slug/item => event handler
+  var emitters = {} // slug/item => event emitter
 
-  module.exports = {startServer};
+  function activate(schedule) {
+    return "emitter"
+  }
 
-}).call(this);
+  function start(slugitem, schedule) {
+    console.log({slugitem, schedule})
+    detectors[slugitem] = {schedule}
+    emitters[slugitem] = activate(schedule)
+  }
+
+  function stop(slugitem) {
+    console.log({slugitem})
+    delete detectors[slugitem]
+    delete emitters[slugitem]
+  }
+
+  function owner(req, res, next) {
+    if(app.securityhandler.isAuthorized(req)) {
+      next()
+    } else {
+      res.status(403).send({
+        error: 'operation reserved for site owner'
+      })
+    }
+  }
+
+  app.get('/plugin/detect/:thing', (req, res) => {
+    var thing = req.params.thing;
+    return res.json({thing});
+  });
+
+  app.post('/plugin/detect/:slug/id/:id/', owner, (req, res) => {
+    let slug = req.params['slug']
+    let item = req.params['id']
+    let slugitem = `${slug}/${item}`
+    let command = req.body
+    console.log('action',command.action||'status',slugitem)
+    if (command.action) {
+      if (command.action == 'start') {
+        start(slugitem, command.schedule)
+      } else if (command.action == 'stop') {
+        stop(slugitem)
+      }
+    }
+    let status = detectors[slugitem] ? 'active' : 'inactive'
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({status}));
+  })
+
+};
+
+module.exports = {startServer};
